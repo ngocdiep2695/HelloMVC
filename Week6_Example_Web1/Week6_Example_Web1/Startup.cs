3e73f3b1316da9501fbe64dfbff66cc5a3bc583e
Điệp Lưu <ip Lu>
2017-04-01 13:59:43 +0700

    init project

---
+++ Week6_Example_Web1/Week6_Example_Web1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week6_Example_Web1.Startup))]
+namespace Week6_Example_Web1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
